2ae7cffb4d41fc84e79f03987984f78cdd693435
Wagner Ramalho <wagner4400@gmail.com>
2024-06-26 20:11:20 -0300

    init

--- quarkus-urlshortener/src/main/java/wsr/codetest/com/repository/UrlRepository.java
+++
@@ -1,9 +0,0 @@
-package wsr.codetest.com.repository;
-
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-import wsr.codetest.com.model.Url;
-
-@ApplicationScoped
-public class UrlRepository implements PanacheMongoRepository<Url> {
-}
--- quarkus-urlshortener/src/test/java/wsr/codetest/com/UrlResourceIT.java
+++
@@ -1,8 +0,0 @@
-package wsr.codetest.com;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class UrlResourceIT extends UrlResourceTest {
-    // Execute the same tests but in packaged mode.
-}
